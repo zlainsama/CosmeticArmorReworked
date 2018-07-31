c26a9f6d5c01f3ad4f6941bfee6e11dfdef3445c
Lain MultipleInstances <zlainsama@163.com>
2018-08-01 01:27:20 +0800

    updates

--- src/main/java/lain/mods/cos/api/CosArmorAPI.java
+++ src/main/java/lain/mods/cos/api/CosArmorAPI.java
@@ -3,17 +3,31 @@ package lain.mods.cos.api;
 import java.util.UUID;
 import lain.mods.cos.CosmeticArmorReworked;
 import lain.mods.cos.api.inventory.CAStacksBase;
-import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CosArmorAPI
 {
 
+    /**
+     * @param uuid
+     *            the UniqueID of a player (See also {@link net.minecraft.entity.player.EntityPlayer#getUniqueID() EntityPlayer.getUniqueID()})
+     * @return associated {@link CAStacksBase CAStacks} for the input uuid
+     */
     public static CAStacksBase getCAStacks(UUID uuid)
     {
-        return isLoaded ? CosmeticArmorReworked.invMan.getCosArmorInventory(uuid).getStacks() : Dummy;
+        return CosmeticArmorReworked.invMan.getCosArmorInventory(uuid).getStacks();
     }
 
-    private static final boolean isLoaded = Loader.isModLoaded("cosmeticarmorreworked");
-    private static final CAStacksBase Dummy = new CAStacksBase();
+    /**
+     * @param uuid
+     *            the UniqueID of a player (See also {@link net.minecraft.entity.player.EntityPlayer#getUniqueID() EntityPlayer.getUniqueID()})
+     * @return associated {@link CAStacksBase CAStacks} for the input uuid on the Client
+     */
+    @SideOnly(Side.CLIENT)
+    public static CAStacksBase getCAStacksClient(UUID uuid)
+    {
+        return CosmeticArmorReworked.invMan.getCosArmorInventoryClient(uuid).getStacks();
+    }
 
 }
--- src/main/java/lain/mods/cos/api/event/CosArmorDeathDrops.java
+++ src/main/java/lain/mods/cos/api/event/CosArmorDeathDrops.java
@@ -2,9 +2,25 @@ package lain.mods.cos.api.event;
 
 import lain.mods.cos.api.inventory.CAStacksBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.Cancelable;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
+/**
+ * This event is fired whenever a player dies and the associated CAStacks is about to be dropped. <br>
+ * <br>
+ * 
+ * {@link #player} contains the instance of EntityPlayer for the event.<br>
+ * {@link #stacks} contains the instance of CAStacks for the player.<br>
+ * <br>
+ * This event is {@link Cancelable}. <br>
+ * If the event is canceled, the CAStacks for the player will not be altered and nothing will be added to the drops.<br>
+ * <br>
+ * This event does not have a result. {@link HasResult}<br>
+ * <br>
+ * This event is fired on the {@link MinecraftForge#EVENT_BUS}.<br>
+ * 
+ */
 @Cancelable
 public class CosArmorDeathDrops extends Event
 {
